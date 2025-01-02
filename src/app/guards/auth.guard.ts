39d1212a3ce1f2fe741eed4cf2faf4fe1aa20ff4
hyl <hanyongle@zgzb.com>
2025-01-02 15:50:02 +0800

    --

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,23 +1,15 @@
-import { Injectable } from '@angular/core';
-import { Router, CanActivate } from '@angular/router';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class AuthGuard implements CanActivate {
-  constructor(
-    private router: Router,
-    private authService: AuthService
-  ) {}
+export const AuthGuard = () => {
+  const router = inject(Router);
+  const authService = inject(AuthService);
 
-  canActivate(): boolean {
+  if (authService.isLoggedIn()) {
     return true;
-    // if (this.authService.isLoggedIn()) {
-    //   return true;
-    // }
-
-    // this.router.navigate(['/login']);
-    // return false;
   }
-} 
+
+  router.navigate(['/login']);
+  return false;
+}; 
